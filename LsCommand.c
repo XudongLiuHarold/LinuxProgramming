db181945fe15bdf23aaf0b13cbba5b26a335ffc6
Harold LIU <Harold_LIU@HaroldLius-MacBook-Pro.local>
2015-10-26 16:42:02 +0800

    update assignment1

--- LsCommand.c
+++ LsCommand.c
@@ -5,8 +5,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<pwd.h>
-#include<grp.h>
 
 
 #define FALSE 0
@@ -30,71 +28,11 @@ int file_select(struct direct *entry)
 	return TRUE;
 }
 
-void get_mode(int mode)
-{
-  if(S_ISDIR(mode))
-		  printf("d");
-  else if(S_ISDIR(mode))
-		  printf("c");
-  else if(S_ISDIR(mode))
-		  printf("b");
-	else 
-			printf("-");
-
-	if (mode & S_IRUSR)
-		printf("r");
-	else 
-		printf("-");
-
-	if (mode & S_IWUSR)
-		printf("w");
-	else 
-		printf("-");
-	if (mode & S_IXUSR)
-		printf("x");
-	else 
-		printf("-");
-
-
-	if (mode & S_IRGRP)
-		printf("r");
-	else 
-		printf("-");
-	if (mode & S_IWGRP)
-		printf("w");
-	else 
-		printf("-");
-	if (mode & S_IXGRP)
-		printf("x");
-	else 
-		printf("-");
-
-
-	if (mode & S_IROTH)
-		printf("r");
-	else 
-		printf("-");
-
-	if (mode & S_IWOTH)
-		printf("w");
-	else 
-		printf("-");
-
-	if (mode & S_IXOTH)
-		printf("x");
-  else
-		printf("-");
-	printf("\t");
-}
-
 
 int main(void)
 {
 
-  char* str ; 
-  scanf("%s",str);
   int count, i;
-  
 	struct direct **files;
 	struct group* g;
 	struct passwd* p;
@@ -108,34 +46,8 @@ int main(void)
 		die("No Files in this Directory\n");
  
  printf("Files Numbers:%d\n",count);
-	if (strcmp(str,"ls")==0)
-  {
- 		 printf("-----------------------------------\n");
+ printf("-----------------------------------\n");
      for(i=1; i< count+1;++i)
        printf("%s\t",files[i-1] ->d_name);
-     printf("\n----------------------------------\n");
-  }
-  else if (strcmp(str,"ls-l")==0)
-	{
-		printf("--------------------------\n");
-		for( i =1;i<count+1;++i)
-			{
-			struct stat st;
-			stat(files[i-1]->d_name, &st);
-			get_mode(st.st_mode);
-		  p = getpwuid(st.st_uid);
-			g = getgrgid(st.st_gid);
-	
- 			
-			printf("%s\t",p->pw_name);
-			printf("%s\t",g->gr_name);
-			printf("%d\t",st.st_size);
-
-			printf("%d\t",localtime(&st.st_mtime));
-			printf("%s\n",files[i-1]->d_name);
-			}
-		printf("-------------------------\n");
-  }
-  else
-   printf("error input\n");
+ printf("\n----------------------------------\n");
 }
